74c960b30513bf9bf500e9992cfbf2ed5752db02
Sunehapodwal <satpalsingh70899@gmail.com>
2025-07-25 14:04:31 +0530

    Update signupscreen.kt

--- app/src/main/java/com/example/testingappproject/signupscreen.kt
+++ app/src/main/java/com/example/testingappproject/signupscreen.kt
@@ -20,72 +20,45 @@ import com.google.firebase.firestore.firestore
 class signupscreen : AppCompatActivity() {
     var btn: Button? = null
     lateinit var binding: ActivitySignupscreenBinding
-            lateinit var auth: FirebaseAuth
+    private lateinit var auth: FirebaseAuth
     private var db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding= ActivitySignupscreenBinding.inflate(layoutInflater)
+        binding = ActivitySignupscreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(
-                systemBars.left,
-                systemBars.top,
-                systemBars.right,
-                systemBars.bottom
-            )
+            v.setPadding(systemBars.left, systemBars.top,systemBars.right, systemBars.bottom)
             insets
         }
 
-        val Uername = findViewById<EditText>(R.id.etuser)
 
-        val phone = findViewById<EditText>(R.id.etphone)
-        val donebutton = findViewById<Button>(R.id.btndone)
-        val userNameStr = Uername.text.toString()
-//        val emailStr = emailt.text.toString().trim()
-//        val passwordStr = password.text.toString().trim()
-        val phoneStr = phone.text.toString()
-
-        donebutton.setOnClickListener {
-
-//            Log.d("auth"," email:$emailt password:$password")
-//
-//            if (userNameStr.isEmpty() || emailt.isEmpty() || password.isEmpty() || phoneStr.isEmpty()) {
-//                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
-//                 return@setOnClickListener
-//            }
+        binding.btndone.setOnClickListener {
             auth = FirebaseAuth.getInstance()
-            auth.createUserWithEmailAndPassword(
-                binding.etemail.text.toString(),
-                binding.etpass.text.toString())
-                .addOnCompleteListener { task ->
-                    if (task.isSuccessful) {
-                        val uid = auth.currentUser?.uid
-                        val user = User(Username = userNameStr, phone = phoneStr)
-                        uid?.let {
-                            db.collection("Users").document(it).set(user)
-                                .addOnSuccessListener {
-                                    Toast.makeText(
-                                        this,
-                                        "registration successful",
-                                        Toast.LENGTH_SHORT
-                                    )
-                                    startActivity(Intent(this, homescren::class.java))
-                                    finish()
-                                }
-                                .addOnFailureListener {
-                                    Toast.makeText(
-                                        this,
-                                         "Failed to save user:${it.message}",
-                                        Toast.LENGTH_SHORT
-                                    ).show()
-                                }
+            val user = binding.etuser.text.toString().trim()
+            val email = binding.etemail.text.toString().trim()
+            val password = binding.etpass.text.toString().trim()
+            val phone = binding.etphone.text.toString().trim()
+            Log.d("Signup","$user $email $password $phone")
+            if (user.isNotEmpty() || email.isNotEmpty() || password.isNotEmpty() || phone.isNotEmpty()) {
+
+                auth.createUserWithEmailAndPassword(
+                    binding.etemail.text.toString(),
+                    binding.etpass.text.toString()
+                )
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "register Successful", Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this, homescren::class.java))
+                            finish()
+                        } else { Toast.makeText(this, "Signup Unsucessful:${task.exception?.message}", Toast.LENGTH_SHORT)
+                                .show()
                         }
-                    } else {
-                        Toast.makeText(this, "Error:${task.exception?.message}", Toast.LENGTH_SHORT)
                     }
-                }
+            }
         }
-    }
-}
+    }}
+
+
+
