4545205f4793fd87b17872c0efcd254fa0da7e38
Sunehapodwal <satpalsingh70899@gmail.com>
2025-07-25 14:04:27 +0530

    Create recyclerdataclass.kt

---
+++ app/src/main/java/com/example/testingappproject/recyclerdataclass.kt
@@ -0,0 +1,11 @@
+package com.example.testingappproject
+
+data class RecyclerDataClass(
+    var id: String?=null,
+    var name: String?=null,
+    var description: String?=null,
+    var email: String?=null,
+    var Contact: String?=null,
+
+
+)
