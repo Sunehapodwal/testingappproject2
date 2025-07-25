7d353e839220c3d497c9dec25f2e5bcbeb6b8a79
Sunehapodwal <satpalsingh70899@gmail.com>
2025-07-25 14:04:08 +0530

    Update homescren.kt

--- app/src/main/java/com/example/testingappproject/homescren.kt
+++ app/src/main/java/com/example/testingappproject/homescren.kt
@@ -1,24 +1,80 @@
 package com.example.testingappproject
 
+import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.testingappproject.databinding.ActivityHomescrenBinding
+import com.example.testingappproject.databinding.DialogopenBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
 
 class homescren : AppCompatActivity() {
-    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var binding: ActivityHomescrenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_homescren)
-        bottomNavigationView=findViewById(R.id.bottomnavigation)
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityHomescrenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.bottomnavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, Homefragment())
+                        .commit()
+                    true
+                }
+                R.id.search_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, SearchFragment())
+                        .commit()
+                    true
+                }
+                R.id.profile_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, ProfileFragment())
+                        .commit()
+                    true
+
+                }
+                R.id.trainig_home-> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, TrainingFragment())
+                        .commit()
+                    true}
+                else -> false
+
+            }
+
+        }
     }
+
+
 }
+
+
+
+
+
+
+
